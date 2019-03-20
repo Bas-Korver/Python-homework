6d09e487cab305b981420ee038690680bde2ee33
Joey Einerhand <j.einerhand@live.nl>
2019-03-20 19:56:52 +0100

    Added new assignment
    With starting op

---
+++ PI4 BLK 3/PI4 part 2/Python_SQL_ActualMembers/Python_SQL_ActualMembers.py
@@ -0,0 +1,11 @@
+#opdracht 5
+import pyodbc
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=..\TennisDatabase.accdb')
+Cursor = conn.cursor()
+
+SelectString = "SELECT * FROM speler WHERE huisnr = '43'"
+ 
+Cursor.execute(SelectString)
+CursorList = Cursor.fetchall()
+
+print (CursorList)
