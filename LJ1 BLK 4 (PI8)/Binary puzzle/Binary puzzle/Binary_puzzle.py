58bcf617d55b6b2c2b84a362b6a2594b9c6e142a
Bas Körver <Bas_Korver@outlook.com>
2019-05-23 22:24:48 +0200

    Added color for user input

--- LJ1 BLK 4 (PI8)/Binary puzzle/Binary puzzle/Binary_puzzle.py
+++ LJ1 BLK 4 (PI8)/Binary puzzle/Binary puzzle/Binary_puzzle.py
@@ -1,1 +1,26 @@
+import os
+os.system('cls')
 
+def prRed(input): return("\033[91m{}\033[00m" .format(input))
+
+def speelveld_generator(moelijkheidsgraad):
+    speelveld = []
+    temp = []
+    grootte = moelijkheidsgraad * 2 + 4
+    for i in range(0, grootte):
+        for x in range(0, grootte):            
+            temp.append('#')
+        speelveld.append(temp)
+        temp = []
+    return(speelveld)
+
+def print_speelveld(speelveld):
+    lines = []    
+    for row in speelveld:
+        lines.append(' '.join(str(x) for x in row))
+    print('\n'.join(lines))
+
+a = speelveld_generator(1)
+b = input('test ')
+a[0][0] = prRed('%s') %(b)
+print_speelveld(a)
