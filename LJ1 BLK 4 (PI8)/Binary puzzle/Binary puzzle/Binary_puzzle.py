ff2e529c373dd393a8620facdfa1cfaaa481067c
Bas Körver <Bas_Korver@outlook.com>
2019-05-26 20:54:57 +0200

    Finished printing section

--- LJ1 BLK 4 (PI8)/Binary puzzle/Binary puzzle/Binary_puzzle.py
+++ LJ1 BLK 4 (PI8)/Binary puzzle/Binary puzzle/Binary_puzzle.py
@@ -1,11 +1,12 @@
 import os
+import time
 os.system('cls')
 
 def prRed(input):
-    return("\033[91m{}\033[00m" .format(input))
+    return('\033[91m{}\033[00m' .format(input))
 
 def prGreen(input):
-    return("\033[92m{}\033[00m" .format(input))
+    return('\033[92m{}\033[00m' .format(input))
 
 def vraag_moeilijkheid():
     error_loop = True
@@ -42,7 +43,7 @@ def speelveld_generator(moelijkheidsgraad):
     grootte = moelijkheidsgraad * 2 + 4
     for i in range(0, grootte):
         for x in range(0, grootte):            
-            temp.append('#')
+            temp.append(prGreen('#'))
         speelveld.append(temp)
         temp = []
 
@@ -55,14 +56,34 @@ def print_speelveld(speelveld):
     print('\n'.join(lines))
 
 def vraag_veld(moeilijkheidsgraad):
-        invul_veld = input('Welk veld wilt u aanpassen?\n' +
-                           'Geef eers de kolom, dan de rij en als laats het cijfer dat u in dat veld wilt invullen.\n' +
-                           'Alles gescheiden door een spatie: ').split()        
-        invul_veld = input_error_correction_veld(invul_veld, moeilijkheidsgraad)   
+    invul_veld = input('Welk veld wilt u aanpassen?\n' +
+                        'Geef eers de kolom, dan de rij en als laats het cijfer dat u in dat veld wilt invullen.\n' +
+                        'Alles gescheiden door een spatie: ').split()        
+    invul_veld = input_error_correction_veld(invul_veld, moeilijkheidsgraad)   
         
-        return(invul_veld)
+    return(invul_veld)
 
 def input_error_correction_veld(invul_veld, moeilijkheidsgraad):
+    grootte = moelijkheidsgraad * 2 + 4
+    error_loop = True
+    if len(invul_veld) < 3:
+        for i in range(0, (3 - len(invul_veld))):
+            invul_veld.append('')
+    if len(invul_veld) > 3:
+        for i in range((3 - len(invul_veld)), 0):
+            del(invul_veld[i])
+        invul_veld[0] = ''
+        invul_veld[1] = ''
+        invul_veld[2] = ''
+
+    while error_loop:        
+        if (len(invul_veld[0]) != 1):
+            invul_veld[0] = input('U heeft iets anders ingetypt bij de kolom letter\n' +
+                                  '         probeer alstublieft opnieuw: ').lower()
+            print()
+        else:
+            error_loop = False
+
     error_loop = True
     while error_loop:
         try:
@@ -70,14 +91,14 @@ def input_error_correction_veld(invul_veld, moeilijkheidsgraad):
         except IndexError:
             invul_veld[0] = input('U heeft iets anders ingetypt bij de kolom letter\n' +
                                   '         probeer alstublieft opnieuw: ').lower()
+            print()
         else:
             error_loop = False   
     invul_veld[0] = (ord(invul_veld[0]) - 97)
 
     error_loop = True
-    while error_loop and invul_veld[0] > moelijkheidsgraad or invul_veld[0] < 0:
-        if invul_veld[0] > moelijkheidsgraad or invul_veld[0] < 0:
-            print(invul_veld)
+    while error_loop and invul_veld[0] > grootte or invul_veld[0] < 0:
+        if invul_veld[0] > moelijkheidsgraad or invul_veld[0] < 0:            
             invul_veld[0] = input('U heeft iets anders ingetypt bij de kolom letter\n' +
                                   '         probeer alstublieft opnieuw: ').lower()
             print()
@@ -90,18 +111,17 @@ def input_error_correction_veld(invul_veld, moeilijkheidsgraad):
     while error_loop:
         try:
             invul_veld[1] = int(invul_veld[1])
-        except (ValueError, IndexError):            
-            print()
+        except (ValueError, IndexError):
             invul_veld[1] = input('U heeft iets anders ingetypt bij de rij nummer\n' +
                                   '        probeer alstublieft opnieuw: ')
-            print()
-            invul_veld[1] -= 1
+            print()            
         else:
-            if invul_veld[1] > moeilijkheidsgraad or invul_veld[1] < 0:
+            if invul_veld[1] > grootte or invul_veld[1] <= 0:
                 invul_veld[1] = input('U heeft iets anders ingetypt bij de rij nummer\n' +
                                       '        probeer alstublieft opnieuw: ')
                 print()               
-            else:            
+            else:
+                invul_veld[1] -= 1
                 error_loop = False
 
 
@@ -110,13 +130,12 @@ def input_error_correction_veld(invul_veld, moeilijkheidsgraad):
         try:
             invul_veld[2] = int(invul_veld[2])
         except (ValueError, IndexError):
-            print()
-            invul_veld[2] = input('U heeft iets anders ingetypt bij de rij nummer\n' +
+            invul_veld[2] = input('U heeft iets anders ingetypt bij het intevulen nummer\n' +
                                   '        probeer alstublieft opnieuw: ')
             print()
         else:
             if invul_veld[2] != 0 and invul_veld[2] != 1:
-                invul_veld[2] = input('U heeft iets anders ingetypt bij de rij nummer\n' +
+                invul_veld[2] = input('U heeft iets anders ingetypt bij het intevulen nummer\n' +
                                       '        probeer alstublieft opnieuw: ')
                 print()
             else:
@@ -126,12 +145,14 @@ def input_error_correction_veld(invul_veld, moeilijkheidsgraad):
     
 
 def veld_controle(speelveld, invul_veld, moeilijkheidsgraad):
+    grootte = moelijkheidsgraad * 2 + 4
     error_loop = True
     while error_loop:
         if isinstance(speelveld[invul_veld[1]][invul_veld[0]], int):
             invul_veld = input('U heeft een veld gekozen waar een al gegenereerde cijfer staat.\n' +
                                'Geef alstublieft opnieuw eers de kolom, dan de rij en als laats het cijfer dat u in dat veld wilt invullen.\n' +
                                'Alles gescheiden door een spatie: ').split()
+            print()
             invul_veld = input_error_correction(invul_veld, moeilijkheidsgraad)
         else:
             speelveld[invul_veld[1]][invul_veld[0]] = prRed(invul_veld[2])
@@ -139,12 +160,21 @@ def veld_controle(speelveld, invul_veld, moeilijkheidsgraad):
 
     return (speelveld)
 
-
-
+start_time = time.time()
+aantal_zetten = 0
 moelijkheidsgraad = vraag_moeilijkheid()
 speelveld = speelveld_generator(moelijkheidsgraad)
 print_speelveld(speelveld)
-while True:
+loop = True
+while loop:
+    aantal_zetten +=1
     invul_veld = vraag_veld(moelijkheidsgraad)
     speelveld = veld_controle(speelveld, invul_veld, moelijkheidsgraad)
     print_speelveld(speelveld)
+    if sum(x.count("\033[92m{#}\033[00m") for x in speelveld) == 0:
+        elapsed_time = time.time() - start_time
+        print('U hebt gekozen voor moeilijkheidsgraad:', moelijkheidsgraad)
+        print()
+        print('Gebruikte zetten:', aantal_zetten)
+        print('en je speeltijd was:', elapsed_time, 'minuten')       
+        loop = False
