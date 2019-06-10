c5f83da77be39cb60a6fe2bd2604e673d970b9bd
Bas Körver <Bas_Korver@outlook.com>
2019-06-10 21:41:24 +0200

    More hanoi 2

--- LJ1 BLK 4 (PI8)/Towers of Hanoi/Main.py
+++ LJ1 BLK 4 (PI8)/Towers of Hanoi/Main.py
@@ -1,5 +1,4 @@
 import tkinter
-import sys
 import turtle
 
 
@@ -22,18 +21,18 @@ class GameStartWindow:
                                              *self.levels)
         self.user_entry.place(x=90, y=35)
         self.submit_button = tkinter.Button(master, text='Submit',
-                                            command=self.action, height=1,
+                                            command=self.submit_function, height=1,
                                             width=10)
         self.submit_button.place(x=10, y=85)
         self.exit_button = tkinter.Button(master, text='Exit',
-                                          command=sys.exit,
+                                          command=exit,
                                           height=1, width=10)
         self.exit_button.place(x=164, y=85)
-        master.bind('<Return>', self.action)
+        master.bind('<Return>', self.submit_function)
 
-    def action(self, event = None):
+    def submit_function(self, event=None):
         self.master.destroy()
-        self.a = self.variable.get()
+        self.chosen_level = self.variable.get()
 
 
 class GameEndWindow:
@@ -47,46 +46,56 @@ class GameEndWindow:
         self.label_one = tkinter.Label(master,
                                        text='You won!')
         self.label_one.place(x=4, y=10)
-        self.submit_button = tkinter.Button(master, text='Restart',
-                                            command=self.action, height=1,
-                                            width=10)
-        self.submit_button.place(x=10, y=85)
+        self.restart_button = tkinter.Button(master, text='Restart',
+                                             command=self.restard_function, height=1,
+                                             width=10)
+        self.restart_button.place(x=10, y=85)
         self.exit_button = tkinter.Button(master, text='Exit',
-                                          command=sys.exit,
+                                          command=exit,
                                           height=1, width=10)
         self.exit_button.place(x=164, y=85)
-        master.bind('<Return>', self.action)
+        master.bind('<Return>', self.restard_function)
 
-    def action(self, event=None):
+    def restard_function(self, event=None):
         self.master.destroy()
-        root = tkinter.Tk()
-        my_gui = GameStartWindow(root)
-        root.mainloop()
-        print(my_gui.a)
 
 
 
-
-
-# Deceleration of global variables
 screen_width_half = 600
 screen_length_half = 350
 pole_list = []
 # Biggest to smallest
 disk_list = []
+move_button_list = []
+
 # debug stuff delete after
 main_window = turtle.Screen()
-main_window.setup(1200, 700, None, None)
+main_window.setup(1200,700,None,None)
 main_window.title("Kokosnoten zijn geen specerijen")
+turtle.speed(0)
+turtle.hideturtle()
 
-
-class PlayingField:
+class Playingfield:
 
     def __init__(self):
         None
 
+class MoveButton:
 
-class PlayingFieldPole:
+    def __init__(self):
+        self.width = 100
+        self.length = 50
+        self.x_pos = 0
+        self.y_pos = 0
+        self.referencing_pole = 0
+
+    def draw_self(self):
+        origin_x = self.x_pos
+        origin_y = self.y_pos
+        fill_color = "green"
+        turtle_draw_a_box(self.length, self.width, origin_x, origin_y, fill_color)
+
+class PlayingfieldPole:
 
     def __init__(self):
         self.length = 140 + (8 * 20)
@@ -138,12 +147,11 @@ def turtle_draw_a_box(length, width, x_pos, y_pos, fill_color):
     turtle.end_fill()
     turtle.penup()
 
-
 def create_poles():
     for i in range(3):
         # Creates the poles in memory
-        pole_list.append(PlayingFieldPole())
-        pole_list[i].x_pos = -310 + (300 * i)
+        pole_list.append(PlayingfieldPole())
+        pole_list[i].x_pos = -300 + (300 * i)
         pole_list[i].y_pos = -150
         #Draws the poles
         pole_list[i].draw_self()
@@ -155,24 +163,59 @@ def create_disks():
         # Creates disks in memory
         disk_list.append(TowerDisk())
         disk_list[i].width = 200 * (1 - (i * 0.1))
-        disk_list[i].x_pos = -310
+        disk_list[i].x_pos = -300
         #20 = height
         disk_list[i].y_pos = -150 + (20 * i)
         disk_list[i].draw_self()
     pole_list[0].pole_content = disk_list
 
-root = tkinter.Tk()
-my_gui = GameStartWindow(root)
-root.mainloop()
-print(my_gui.a)
+def create_buttons():
 
+    for i in range(3):
+         # Creates the buttons in memory
+        move_button_list.append(MoveButton())
+        move_button_list[i].x_pos = -300 + (300 * i)
+        move_button_list[i].y_pos = -300
+        move_button_list[i].referencing_pole = pole_list[i]
+        # Draws the buttons
+        move_button_list[i].draw_self()
+
+def draw_disks():
+    for pole in range(3):
+        for disk in pole_list[pole].pole_content:
+            if disk:
+                disk.draw_self()
+
+def check_button_clicked(x,y):
+    # instead of x going from -600 to 600, from 0 to 1200
+    # +40 so it takes into account button offset from edge of screen
+    column_button = (x + 600 + ) // 100
 
 # Code excecution starts here
+
+while True:
+    root = tkinter.Tk()
+    start_window = GameStartWindow(root)
+    root.mainloop()
+
+    input()
+
+    root = tkinter.Tk()
+    end_window = GameEndWindow(root)
+    root.mainloop()
+
+!!!!!
+## level variabelen
+GameStartWindow.chosen_level
+
 
 turtle.penup()
 create_poles()
 create_disks()
+draw_disks()
+create_buttons()
 print(pole_list[0].pole_content)
 print(disk_list)
+turtle.onclick(check_button_clicked)
 # This makes sure the program doesn't quit on its own
 turtle.mainloop()
--- LJ1 BLK 4 (PI8)/Towers of Hanoi/temp2.py
+++ LJ1 BLK 4 (PI8)/Towers of Hanoi/temp2.py
@@ -8,19 +8,37 @@ screen_length_half = 350
 pole_list = []
 # Biggest to smallest
 disk_list = []
+move_button_list = []
+
+
 # debug stuff delete after
 main_window = turtle.Screen()
-main_window.setup(1200, 700, None, None)
+main_window.setup(1200,700,None,None)
 main_window.title("Kokosnoten zijn geen specerijen")
+turtle.speed(0)
+turtle.hideturtle()
 
-
-class PlayingField:
+class Playingfield():
 
     def __init__(self):
         None
 
+class MoveButton():
 
-class PlayingFieldPole:
+    def __init__(self):
+        self.width = 100
+        self.length = 50
+        self.x_pos = 0
+        self.y_pos = 0
+        self.referencing_pole = 0
+
+    def draw_self(self):
+        origin_x = self.x_pos
+        origin_y = self.y_pos
+        fill_color = "green"
+        turtle_draw_a_box(self.length, self.width, origin_x, origin_y, fill_color)
+
+class PlayingfieldPole():
 
     def __init__(self):
         self.length = 140 + (8 * 20)
@@ -38,7 +56,7 @@ class PlayingFieldPole:
         turtle_draw_a_box(self.length, 20, origin_x, origin_y, fill_color)
 
 
-class TowerDisk:
+class TowerDisk():
 
     def __init__(self):
         # Calculated in create_disks
@@ -72,12 +90,11 @@ def turtle_draw_a_box(length, width, x_pos, y_pos, fill_color):
     turtle.end_fill()
     turtle.penup()
 
-
 def create_poles():
     for i in range(3):
         # Creates the poles in memory
-        pole_list.append(PlayingFieldPole())
-        pole_list[i].x_pos = -310 + (300 * i)
+        pole_list.append(PlayingfieldPole())
+        pole_list[i].x_pos = -300 + (300 * i)
         pole_list[i].y_pos = -150
         #Draws the poles
         pole_list[i].draw_self()
@@ -87,13 +104,35 @@ def create_disks():
 
     for i in range(8):
         # Creates disks in memory
-        disk_list.append(TowerDisk((200 * (1 - (i * 0.1))), -310, (-150 + (20 * i))))
-        # disk_list[i].width = 200 * (1 - (i * 0.1))
-        # disk_list[i].x_pos = -310
+        disk_list.append(TowerDisk())
+        disk_list[i].width = 200 * (1 - (i * 0.1))
+        disk_list[i].x_pos = -300
         #20 = height
-        # disk_list[i].y_pos = -150 + (20 * i)
+        disk_list[i].y_pos = -150 + (20 * i)
         disk_list[i].draw_self()
     pole_list[0].pole_content = disk_list
+
+def create_buttons():
+
+    for i in range(3):
+         # Creates the buttons in memory
+        move_button_list.append(MoveButton())
+        move_button_list[i].x_pos = -300 + (300 * i)
+        move_button_list[i].y_pos = -300
+        move_button_list[i].referencing_pole = pole_list[i]
+        # Draws the buttons
+        move_button_list[i].draw_self()
+
+def draw_disks():
+    for pole in range(3):
+        for disk in pole_list[pole].pole_content:
+            if disk:
+                disk.draw_self()
+
+def check_button_clicked(x,y):
+    # instead of x going from -600 to 600, from 0 to 1200
+    # +40 so it takes into account button offset from edge of screen
+    column_button = (x + 600 + ) // 100
 
 
 # Code excecution starts here
@@ -101,7 +140,10 @@ def create_disks():
 turtle.penup()
 create_poles()
 create_disks()
+draw_disks()
+create_buttons()
 print(pole_list[0].pole_content)
 print(disk_list)
+turtle.onclick(check_button_clicked)
 # This makes sure the program doesn't quit on its own
 turtle.mainloop()
