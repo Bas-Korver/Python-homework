1f8bbd753603b842d843f48ae25633b9eef2e740
Bas Körver <Bas_Korver@outlook.com>
2019-06-11 07:45:24 +0200

    More hanoi 4

--- LJ1 BLK 4 (PI8)/Towers of Hanoi/Main.py
+++ LJ1 BLK 4 (PI8)/Towers of Hanoi/Main.py
@@ -1,6 +1,8 @@
 import tkinter
 import turtle
-
+import time
+import sys
+import os
 
 class GameStartWindow:
     def __init__(self, master):
@@ -10,18 +12,19 @@ class GameStartWindow:
         master.geometry("254x120+{}+{}".format(x_coordinate, y_coordinate))
         master.title('Towers of Hanoi')
 
-        self.levels = [6, 8]
+        self.levels = [3, 4, 5, 6, 7, 8, 9]
         self.label_one = tkinter.Label(master,
-                                       text='Choose Level, 6 = six rings and '
-                                            '8 = eight rings')
+                                       text='Choose your Level. Level 3 '
+                                            'being three rings, etc')
         self.label_one.place(x=4, y=10)
         self.variable = tkinter.StringVar(master)
-        self.variable.set("6")
+        self.variable.set(3)
         self.user_entry = tkinter.OptionMenu(master, self.variable,
                                              *self.levels)
         self.user_entry.place(x=90, y=35)
         self.submit_button = tkinter.Button(master, text='Submit',
-                                            command=self.submit_function, height=1,
+                                            command=self.submit_function,
+                                            height=1,
                                             width=10)
         self.submit_button.place(x=10, y=85)
         self.exit_button = tkinter.Button(master, text='Exit',
@@ -36,8 +39,10 @@ class GameStartWindow:
 
 
 class GameEndWindow:
-    def __init__(self, master):
+    def __init__(self, master, moves_made, start_time):
         self.master = master
+        self.moves_made = moves_made
+        self.elapsed_time = round(((time.time() - start_time) / 60), 2)
         x_coordinate = int((master.winfo_screenwidth() / 2) - 127)
         y_coordinate = int((master.winfo_screenheight() / 2) - 60)
         master.geometry("254x120+{}+{}".format(x_coordinate, y_coordinate))
@@ -46,39 +51,149 @@ class GameEndWindow:
         self.label_one = tkinter.Label(master,
                                        text='You won!')
         self.label_one.place(x=4, y=10)
+        self.variable_one = tkinter.StringVar(master)
+        self.variable_one.set('You have made {} moves '.format(self.moves_made))
+        self.label_two = tkinter.Label(master, textvariable=self.variable_one)
+        self.label_two.place(x=4, y=35)
+        self.variable_two = tkinter.StringVar(master)
+        self.variable_two.set('and you have taken {} minute(s)'.format(self.elapsed_time))
+        self.label_three = tkinter.Label(master, textvariable=self.variable_two)
+        self.label_three.place(x=4, y=50)
         self.restart_button = tkinter.Button(master, text='Restart',
-                                             command=self.restard_function, height=1,
+                                             command=self.restart_function,
+                                             height=1,
                                              width=10)
         self.restart_button.place(x=10, y=85)
         self.exit_button = tkinter.Button(master, text='Exit',
                                           command=exit,
                                           height=1, width=10)
         self.exit_button.place(x=164, y=85)
-        master.bind('<Return>', self.restard_function)
+        master.bind('<Return>', self.restart_function)
 
-    def restard_function(self, event=None):
+    def restart_function(self, event=None):
         self.master.destroy()
 
 
 
-screen_width_half = 600
-screen_length_half = 350
-pole_list = []
-# Biggest to smallest
-disk_list = []
-move_button_list = []
-
-# debug stuff delete after
-main_window = turtle.Screen()
-main_window.setup(1200,700,None,None)
-main_window.title("Kokosnoten zijn geen specerijen")
-turtle.speed(0)
-turtle.hideturtle()
-
-class Playingfield:
+class DiskMoveHelper:
 
     def __init__(self):
-        None
+        # Tower which the disks is being moved from
+        self.origin_tower = False
+        self.destination_tower = False
+        self.button_pressed = False
+
+    def button_zero(self, x, y):
+        print("kms")
+        self.button_pressed = 0
+        self.select_destination_origin()
+
+    def button_one(self, x, y):
+        print("mks")
+        self.button_pressed = 1
+        self.select_destination_origin()
+
+    def button_two(self, x, y):
+        self.button_pressed = 2
+        self.select_destination_origin()
+
+    def select_destination_origin(self):
+        if self.origin_tower == False:
+            self.origin_tower = pole_list[self.button_pressed]
+            print(self.origin_tower)
+        else:
+            self.destination_tower = pole_list[self.button_pressed]
+            print(self.destination_tower)
+            if self.origin_tower != self.destination_tower:
+                self.move_disks()
+            else:
+                print(
+                    "Sorry, can't move a disk to a position where it already "
+                    "is!")
+                text_turtle.goto(0, 300)
+                text_turtle.pendown()
+                text_turtle.write(
+                    "Sorry, can't move a disk to a position where it already "
+                    "is!",
+                    align="center", font=("Arial", 20))
+                text_turtle.penup()
+
+                time.sleep(3)
+                text_turtle.clear()
+                self.cleanup()
+
+    def move_disks(self):
+        global text_turtle
+        global moves_made
+        moving_disk_turtle = self.origin_tower.pole_content[-1].disk_turtle
+        moving_disk = self.origin_tower.pole_content[-1]
+
+        # if the destination pole is not empty, assign variables
+        # Variables needed for next if statement
+        # crashes without if statement
+        if (self.destination_tower.pole_content):
+            width1 = self.origin_tower.pole_content[-1].width
+            width2 = self.destination_tower.pole_content[-1].width
+
+        # if the destination is empty, or if the lowest disk in the destination
+        # has a bigger width than the disk you want to move to that place
+        if (not self.destination_tower.pole_content) or (width1 < width2):
+            print("in move_disks")
+            self.origin_tower.pole_content.pop()
+            self.destination_tower.pole_content.append(moving_disk)
+            x_pos = self.destination_tower.x_pos
+            position_in_tower = self.destination_tower.pole_content.index(
+                moving_disk)
+            y_pos = -90 + (20 * position_in_tower)
+            moving_disk_turtle.goto(x_pos, y_pos)
+            moves_made += 1
+        else:
+            print("Sorry, the disk on the destination pole is smaller")
+            text_turtle.goto(0, 300)
+            text_turtle.pendown()
+            text_turtle.write(
+                "Sorry, the disk on the destination pole is smaller.",
+                align="center", font=("Arial", 20))
+            text_turtle.penup()
+
+            time.sleep(3)
+            text_turtle.clear()
+
+        self.wincondition_check()
+
+    def cleanup(self):
+        self.origin_tower = False
+        self.destination_tower = False
+        self.button_pressed = False
+        print("cleanup done")
+
+    def wincondition_check(self):
+        global start_time
+        # checkt of winconditie is bereikt
+        # if the pole that has the tower is not the starter tower (avoid cheat)
+        # and if the pole that has the tower is the same as the list with
+        # disks (so if the pole has all disks, in order)
+        if ((self.destination_tower != pole_list[0]) and (
+                self.destination_tower.pole_content == disk_list)):
+            # events that trigger after the user
+            main_window.bye()
+            root = tkinter.Tk()
+            end_window = GameEndWindow(root, moves_made, start_time)
+            root.mainloop()
+            python = sys.executable
+            for name in dir():
+                if not name.startswith('_'):
+                    del globals()[name]
+
+            for name in dir():
+                if not name.startswith('_'):
+                    del locals()[name]
+
+        else:
+            # events that trigger when the user hasnt won yet
+            print("haven't won yet")
+        self.cleanup()
+
 
 class MoveButton:
 
@@ -89,28 +204,32 @@ class MoveButton:
         self.y_pos = 0
         self.referencing_pole = 0
 
-    def draw_self(self):
-        origin_x = self.x_pos
-        origin_y = self.y_pos
-        fill_color = "green"
-        turtle_draw_a_box(self.length, self.width, origin_x, origin_y, fill_color)
+    def draw_self(self, inst_turtle):
+        inst_turtle.penup()
+        inst_turtle.fillcolor("green")
+        inst_turtle.goto(self.x_pos, self.y_pos)
+        inst_turtle.pendown()
+        inst_turtle.shape("square")
+        inst_turtle.shapesize(2, 8)
 
-class PlayingfieldPole:
+
+class PlayingFieldPole:
 
     def __init__(self):
-        self.length = 140 + (8 * 20)
+        self.length = 140 + (amount_of_disks * 20)
         # Default width is 20
         self.pole_content = []
         # Absolute coordinates in Screen
         self.x_pos = 0
         self.y_pos = 0
 
-    def draw_self(self):
-        # An "origin" is the point where the turtle starts drawing the image
-        origin_x = self.x_pos
-        origin_y = self.y_pos
-        fill_color = "black"
-        turtle_draw_a_box(self.length, 20, origin_x, origin_y, fill_color)
+    def draw_self(self, inst_turtle):
+        inst_turtle.penup()
+        inst_turtle.fillcolor("black")
+        inst_turtle.goto(self.x_pos, self.y_pos)
+        inst_turtle.pendown()
+        inst_turtle.shape("square")
+        inst_turtle.shapesize(10, 1)
 
 
 class TowerDisk:
@@ -121,101 +240,115 @@ class TowerDisk:
         # Default height is 20
         self.x_pos = 0
         self.y_pos = 0
+        self.disk_turtle = None
 
-    def draw_self(self):
-        # An "origin" is the point where the turtle starts drawing the image
-        origin_x = self.x_pos
-        origin_y = self.y_pos
-        fill_color = "Blue"
-        turtle_draw_a_box(20, self.width, origin_x, origin_y, fill_color)
+    def draw_self(self, inst_turtle, i):
+        inst_turtle.penup()
+        inst_turtle.fillcolor(255 - (i * 30), 255 // (i + 1 * 20),
+                              0 + (i * 30))
+        inst_turtle.goto(self.x_pos, self.y_pos)
+        inst_turtle.pendown()
+        inst_turtle.shape("square")
+        inst_turtle.shapesize(1, 7 - (0.5 * i))
 
-# Assuming origin is in the middle of the bottom line
-def turtle_draw_a_box(length, width, x_pos, y_pos, fill_color):
-    turtle.fillcolor(fill_color)
-    turtle.setpos(x_pos, y_pos)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.forward(width / 2)
-    turtle.left(90)
-    turtle.forward(length)
-    turtle.left(90)
-    turtle.forward(width)
-    turtle.left(90)
-    turtle.forward(length)
-    turtle.left(90)
-    turtle.forward(width / 2)
-    turtle.end_fill()
-    turtle.penup()
 
 def create_poles():
     for i in range(3):
         # Creates the poles in memory
-        pole_list.append(PlayingfieldPole())
+        pole_list.append(PlayingFieldPole())
         pole_list[i].x_pos = -300 + (300 * i)
-        pole_list[i].y_pos = -150
-        #Draws the poles
-        pole_list[i].draw_self()
+        pole_list[i].y_pos = 0
+
+        # Turtle
+        pole_turtle_list.append(turtle.Turtle(visible=True))
+        pole_turtle_list[i].speed("fastest")
+        pole_list[i].draw_self(pole_turtle_list[i])
 
 
 def create_disks():
-
-    for i in range(8):
+    for i in range(amount_of_disks):
         # Creates disks in memory
         disk_list.append(TowerDisk())
-        disk_list[i].width = 200 * (1 - (i * 0.1))
+        disk_list[i].width = amount_of_disks - (1 * i)
         disk_list[i].x_pos = -300
-        #20 = height
-        disk_list[i].y_pos = -150 + (20 * i)
-        disk_list[i].draw_self()
-    pole_list[0].pole_content = disk_list
+        # 20 = height
+        disk_list[i].y_pos = -90 + (20 * i)
+
+        # turtle
+        disk_turtle_list.append(turtle.Turtle(visible=True))
+        disk_turtle_list[i].speed("fastest")
+        disk_list[i].draw_self(disk_turtle_list[i], i)
+
+        # UML: makes disk aware of turtle
+        disk_list[i].disk_turtle = disk_turtle_list[i]
+
+        disk_turtle_list[i].penup()
+    pole_list[0].pole_content = list(disk_list)
+
 
 def create_buttons():
-
     for i in range(3):
-         # Creates the buttons in memory
+        # Creates the buttons in memory
         move_button_list.append(MoveButton())
         move_button_list[i].x_pos = -300 + (300 * i)
-        move_button_list[i].y_pos = -300
+        move_button_list[i].y_pos = -200
         move_button_list[i].referencing_pole = pole_list[i]
-        # Draws the buttons
-        move_button_list[i].draw_self()
 
-def draw_disks():
-    for pole in range(3):
-        for disk in pole_list[pole].pole_content:
-            if disk:
-                disk.draw_self()
+        # Create turtle for each instance
+        move_button_turtle_list.append(turtle.Turtle(visible=True))
+        move_button_turtle_list[i].speed("fastest")
+        move_button_list[i].draw_self(move_button_turtle_list[i])
 
-def check_button_clicked(x,y):
+
+def check_button_clicked():
     # instead of x going from -600 to 600, from 0 to 1200
-    # +40 so it takes into account button offset from edge of screen
-    column_button = (x + 600 + ) // 100
+    # -50 so it takes into account button offset from edge of screen
+    move_button_turtle_list[0].onclick(DiskMoveHelper.button_zero)
+    move_button_turtle_list[1].onclick(DiskMoveHelper.button_one)
+    move_button_turtle_list[2].onclick(DiskMoveHelper.button_two)
 
-# Code excecution starts here
+
+def main():
+    global start_time
+    start_time = time.time()
+    create_poles()
+    create_disks()
+    # draw_disks()
+    create_buttons()
+    print(pole_list[0].pole_content)
+    print(disk_turtle_list)
+    check_button_clicked()
+    # This makes sure the program doesn't quit on its own
+    turtle.mainloop()
+
 
 while True:
+    # Pop-up window for level selection
     root = tkinter.Tk()
     start_window = GameStartWindow(root)
     root.mainloop()
 
-    input()
+    # Deceleration of global variables
+    moves_made = 0
 
-    root = tkinter.Tk()
-    end_window = GameEndWindow(root)
-    root.mainloop()
+    pole_list = []
+    pole_turtle_list = []
 
-!!!!!
-## level variabelen
-GameStartWindow.chosen_level
+    # Biggest to smallest
+    disk_list = []
+    disk_turtle_list = []
 
+    move_button_list = []
+    move_button_turtle_list = []
 
-turtle.penup()
-create_poles()
-create_disks()
-draw_disks()
-create_buttons()
-print(pole_list[0].pole_content)
-print(disk_list)
-turtle.onclick(check_button_clicked)
-# This makes sure the program doesn't quit on its own
-turtle.mainloop()
+    amount_of_disks = int(start_window.chosen_level)
+
+    main_window = turtle.Screen()
+    main_window.setup(1200, 700, None, None)
+    main_window.title("Towers of Hanoi")
+    main_window.colormode(255)
+    text_turtle = turtle.Turtle()
+    text_turtle.hideturtle()
+    turtle.speed('fastest')
+    DiskMoveHelper = DiskMoveHelper()
+    main()
